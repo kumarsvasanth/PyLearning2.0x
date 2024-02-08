e8c83c23f53b13e3e3dc7e63c9bd6565d81ddf56
kumarsvasanth <vasanthkumars887@gmail.com>
2024-02-08 22:51:09 +0530

    Assignment

---
+++ 07_07_Feb2024/Task2-2.py
@@ -0,0 +1,9 @@
+"""Create a program that takes two numbers
+as input and prints whether the first number is greater than,less than
+or eaual to the second number"""
+
+a = int(input("1st input"))
+b = int(input("2nd input"))
+print(a > b)
+print(a < b)
+print(a == b)
---
+++ 07_07_Feb2024/Task2-3.py
@@ -0,0 +1,8 @@
+"""Develop a Python script that calculates the
+ square and cube of a given number"""
+
+num1 = 10;
+square = num1**2
+cube = num1**3
+print(f"Square of a number 10 is {square}")
+print(f"Cube of a number 10 is {cube}")
